135c8d93d4cd2d765c979af269bd6c7d2db63887
foxalan <33643467+foxalan@users.noreply.github.com>
2018-06-05 10:36:01 +0800

    finish base bottom delegate

---
+++ core/src/main/java/com/example/core/delegate/bottom/BottomTabBean.java
@@ -0,0 +1,35 @@
+package com.example.core.delegate.bottom;
+
+/**
+ * @author alan
+ * 底部的Bean 使用字体图标
+ */
+
+public final class BottomTabBean {
+
+    private final CharSequence ICON;
+    private final CharSequence TITLE;
+
+    public BottomTabBean(CharSequence icon, CharSequence title) {
+        this.ICON = icon;
+        this.TITLE = title;
+    }
+
+    public CharSequence getIcon() {
+        return ICON;
+    }
+
+    public CharSequence getTitle() {
+        return TITLE;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
