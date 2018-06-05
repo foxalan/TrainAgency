2cb4df51e6b3642f7b630393bc76f9401f8a4aa7
foxalan <33643467+foxalan@users.noreply.github.com>
2018-06-05 11:36:27 +0800

    remove actionbar and  add statues bar

--- app/src/main/java/com/example/alan/trainagency/MainActivity.java
+++ app/src/main/java/com/example/alan/trainagency/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.alan.trainagency;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 
 import com.example.core.activity.ProxyActivity;
+import com.example.core.app.Latte;
 import com.example.core.delegate.LatteDelegate;
 import com.example.ec.main.EcBottomDelegate;
+
+import qiu.niorgai.StatusBarCompat;
 
 /**
  * @author alan
@@ -22,6 +26,14 @@ public class MainActivity extends ProxyActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //去年ActionBar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        Latte.getConfigurator().withActivity(this);
+        //设置沉浸式状态栏
+        StatusBarCompat.translucentStatusBar(this, true);
 
     }
 
