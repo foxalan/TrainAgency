7f62423a606b28caae5d3f02e73896732223e614
foxalan <33643467+foxalan@users.noreply.github.com>
2018-06-22 15:53:01 +0800

    finish class delegate

---
+++ ec/src/main/java/com/example/ec/main/home/subject/list/SubjectListDelegate.java
@@ -0,0 +1,28 @@
+package com.example.ec.main.home.subject.list;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.example.core.delegate.LatteDelegate;
+
+/**
+ * @author alan
+ *         Date  2018/6/22.
+ *         Function : 具体科目的培训班信息
+ *         Issue :
+ */
+
+public class SubjectListDelegate extends LatteDelegate {
+
+    @Override
+    public Object setLayout() {
+        return null;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
+
+    }
+}
